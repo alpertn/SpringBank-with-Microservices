da153d94daa1ecbcb761a35edc504e14d3e12e8d
alpertn <ozbaymehmetalper@gmail.com>
2026-01-19 03:27:30 +0300

    servis guncellemesi

--- money-service/src/main/java/com/banking_microservices/money_service/controller/controller.java
+++ money-service/src/main/java/com/banking_microservices/money_service/controller/controller.java
@@ -1,12 +1,33 @@
 package com.banking_microservices.money_service.controller;
 
+import com.banking_microservices.money_service.dto.IdDto;
+import com.banking_microservices.money_service.repository.repository;
+import com.banking_microservices.money_service.service.service;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import lombok.Value;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 
 @RequiredArgsConstructor
 @RestController
-@RequestMapping("/api/v1")
+@RequestMapping("/api")
 public class controller {
+
+
+    private final repository repository;
+    private final service service;
+
+    @GetMapping("/health")
+    public ResponseEntity<String> health() {
+        return ResponseEntity.ok("Money service is healty");
+    }
+
+    @PostMapping("/createusermoney")
+    public ResponseEntity<?> userOlustur(@NotNull @RequestBody String userId){
+        return ResponseEntity.ok(service.generateUser(userId));
+    }
+
 }
