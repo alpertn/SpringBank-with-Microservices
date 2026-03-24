790b7176e42c981d1fe8fa8464cd57a59a430625
alpertn <ozbaymehmetalper@gmail.com>
2026-03-24 20:48:30 +0300

    Transaction Service Clean code guncellemesi service helper eklenmesi kafka gereksiz JavaDoc yorumlarının SUANLIK silinmesi gelecekte eklenecek.

--- fraud-service/src/main/java/com/banking_microservices/fraud_service/dto/enums/TransactionStatus.java
+++ fraud-service/src/main/java/com/banking_microservices/fraud_service/dto/enums/TransactionStatus.java
@@ -8,18 +8,13 @@ import lombok.RequiredArgsConstructor;
 public enum TransactionStatus {
     CREATED("İşleminiz alındı"),
     VALIDATION_PENDING("Hesap bilgileri doğrulanıyor"),
-    VALIDATION_FAILED("Hesap bilgileri doğrulanamadı"),
-    INSUFFICIENT_FUNDS("Yetersiz bakiye"),
     FRAUD_REVIEW("İşleminiz inceleniyor"),
-    FRAUD_REJECTED("İşlem güvenlik kontrolünden geçemedi"),
     BLOCK_MONEY("Tutar rezerve edildi"),
     BLOCK_MONEY_FAILED("Tutar rezerve edilemedi, işlem iptal edildi"),
-    PROCESSING("Transfer işleniyor"),
     COMPLETED("İşlem tamamlandı"),
     FAILED("İşlem başarısız"),
-    KAFKA_ERROR("Teknik bir hata oluştu"),
-    DECLINED("İşlem reddedildi"),
-    REVERSED("İşlem geri alındı");
+    DEPOSIT_FAILED("Para yatırma işlemi başarısız"),
+    WITHDRAW_FAILED("Para çekme işlemi başarısız");
 
     private final String description;
 }
