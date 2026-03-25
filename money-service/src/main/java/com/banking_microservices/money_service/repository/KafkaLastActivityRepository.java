f426b244d0752a11707b48a22929986a90ae91ec
alpertn <ozbaymehmetalper@gmail.com>
2026-03-26 02:18:59 +0300

    kubernet Dosyalarindaki HPA cpu pods memory limitleri yariya kadar dusurulmesi. 02.16 26.03.26

--- money-service/src/main/java/com/banking_microservices/money_service/repository/KafkaLastActivityRepository.java
+++ money-service/src/main/java/com/banking_microservices/money_service/repository/KafkaLastActivityRepository.java
@@ -3,7 +3,9 @@ package com.banking_microservices.money_service.repository;
 import com.banking_microservices.money_service.models.KafkaLastActivity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface KafkaLastActivityRepository extends JpaRepository<KafkaLastActivity, String> {
     boolean existsByEventUUID(String eventUUID);
     boolean existsByEventUUIDAndEventType(String eventUUID, String eventType);
